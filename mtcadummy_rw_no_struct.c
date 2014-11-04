83b765306d9a639f4dd70951d38ea40b1eff6b04
killenb <killenb@5f601a5c-88b9-47f3-b851-818f6741d00d>
2014-11-04 17:22:25 +0000

    - copied pcieuni_io_compat from MtcaMappedDevice - moved check and bar calculation to a separate function
    git-svn-id: https://svnsrv.desy.de/desy/mtca4u/drivers/mtcadummy/trunk@625 5f601a5c-88b9-47f3-b851-818f6741d00d

--- mtcadummy_rw_no_struct.c
+++ mtcadummy_rw_no_struct.c
@@ -10,22 +10,29 @@
 #include "mtcadummy.h"
 #include "mtcadummy_firmware.h"
 
-ssize_t mtcaDummy_write_no_struct(struct file *filp, const char __user *buf, 
-				  size_t count, loff_t *f_pos)
-{
-  mtcaDummyData *deviceData;
+/** A struct which contains information about the transfer which is to be performed.
+ *  Used to get data out of a common calculate and check function for read and write.
+ *  Not all variables are needed later, but they are kept in case they are needed in future
+ *  because they have to be calculated anyway for the checks.
+ */
+typedef struct _transfer_information{
   unsigned int bar;
   unsigned long offset; /* offset inside the bar */
   unsigned long barSizeInBytes;
   u32 * barStart, * barEnd;
   unsigned int nBytesToTransfer;
+} transfer_information;
 
-  deviceData = filp->private_data;
-  
-  /* ### sanity and input checks ### */
+
+/** Checks the input and calculates the transfer information.
+ *  Returns a negative value in case of error, 0 upon success.
+ */
+int checkAndCalculateTransferInformation( mtcaDummyData const * deviceData,
+					  size_t count, loff_t virtualOffset,
+					  transfer_information * transferInformation ){
 
   /* check the input data. Only 32 bit reads are supported */
-  if ( (*f_pos)%4 ){
+  if ( virtualOffset%4 ){
     printk("%s\n", "Incorrect position, has the be a multiple of 4");
     return -EFAULT;
   }
@@ -34,57 +41,101 @@ ssize_t mtcaDummy_write_no_struct(struct file *filp, const char __user *buf,
     return -EFAULT;
   }
 
-  printk("mtcaDummy_write_no_struct: count %zx , f_pos %Lx\n", count, (*f_pos) );
+  /*  printk("mtcadummy::checkAndCalculateTransferInformation: count %zx , virtualOffsets %Lx\n", count, virtualOffset );
+   */
 
   /* Before locking the mutex check if the request is valid (do not write after the end of the bar). */
   /* Do not access the registers, only check the pointer values without locking the mutex! */
   
   /* determine the bar from the f_pos */
-  bar = ((*f_pos) >> 60) & 0x7;
+  transferInformation->bar = (virtualOffset >> 60) & 0x7;
   /* mask out the bar position from the offset */
-  offset = (*f_pos) & 0x0FFFFFFFFFFFFFFFL;
-
-  printk("mtcaDummy_write_no_struct: bar %x, offset %lx\n", bar, offset);  
+  transferInformation->offset = virtualOffset & 0x0FFFFFFFFFFFFFFFL;
   
+  /*  printk("mtcadummy::checkAndCalculateTransferInformation: bar %x, offset %lx\n",
+	    transferInformation->bar,
+	    transferInformation->offset);  
+  */
+
   /* get the bar's start and end address */
-  /* it is safe to access deviceData->systemBar without the mutex because it
-     is never written anywhere */
-  switch (bar){
+  /* FIXME: organise the information as arrays, not as individual variables, and you might get rid of this block */
+  switch (transferInformation->bar){
   case 0:
-    barStart = deviceData->systemBar;
-    barSizeInBytes = MTCADUMMY_N_REGISTERS*4;
+    transferInformation->barStart = deviceData->systemBar;
+    transferInformation->barSizeInBytes = MTCADUMMY_N_REGISTERS*4;
     break;
   case 2:
-    barStart = deviceData->dmaBar;
-    barSizeInBytes = MTCADUMMY_DMA_SIZE;
+    transferInformation->barStart = deviceData->dmaBar;
+    transferInformation->barSizeInBytes = MTCADUMMY_DMA_SIZE;
     break;
   case 1:
   case 3:
   case 4:
   case 5:
-    barStart = 0;
-    barSizeInBytes = 0;
+    transferInformation->barStart = 0;
+    transferInformation->barSizeInBytes = 0;
     
   default:
-    printk("MTCADUMMY_WRITE_NO_STRUCT: Invalid bar number %d\n", bar);
+    printk("MTCADUMMY_WRITE_NO_STRUCT: Invalid bar number %d\n", 
+	   transferInformation->bar);
+    return -EFAULT;
+  }  
+
+  if (!transferInformation->barStart){
+    printk("BAR %d not implemented in this device",  transferInformation->bar);
     return -EFAULT;
   }
-  /* When adding to a pointer, the + operator expects the number of items, not the size in bytes */
-  barEnd = barStart + barSizeInBytes/sizeof(u32);
- 
-  printk("mtcaDummy_write_no_struct: barStart %p, barSize %lx, barEnd %p\n", barStart, barSizeInBytes, barEnd);  
- 
+
+  /* When adding to a pointer, the + operator expects number of items, not the size in bytes */
+  transferInformation->barEnd = transferInformation->barStart + 
+    transferInformation->barSizeInBytes/sizeof(u32);
+
+  /*
+  printk("mtcadummy::checkAndCalculateTransferInformation: barStart %p, barSize %lx, barEnd %p\n",
+	    transferInformation->barStart, transferInformation->barSizeInBytes,
+	    transferInformation->barEnd);  
+  */
+
   /* check that writing does not start after the end of the bar */
-  if ( offset > barSizeInBytes ){
+  if ( transferInformation->offset > transferInformation->barSizeInBytes ){
     printk("%s\n", "Cannot start writing after the end of the bar.");
     return -EFAULT;
   }
 
   /* limit the number of transferred by to the end of the bar. */
   /* The second line is safe because we checked before that offset <= barSizeInBytes */
-  nBytesToTransfer = ( barSizeInBytes < offset + count ?
-		       barSizeInBytes - offset  :
-		       count);
+  transferInformation->nBytesToTransfer = 
+    ( (transferInformation->barSizeInBytes < transferInformation->offset + count) ?
+      transferInformation->barSizeInBytes - transferInformation->offset  :
+      count);
+
+  /*
+  printk("mtcadummy::checkAndCalculateTransferInformation:  nBytesToTransfer %x, count %lx",
+	 transferInformation->nBytesToTransfer, count);
+  */
+
+  return 0;
+}
+
+ssize_t mtcaDummy_write_no_struct(struct file *filp, const char __user *buf, 
+				  size_t count, loff_t *f_pos)
+{
+  mtcaDummyData *deviceData;
+  transfer_information transferInformation;
+  int transferInfoError;
+
+  deviceData = filp->private_data;
+  
+  /* The checkAndCalculateTransferInformation is only accessing static data in the 
+     deviceData struct. No need to hold the mutex.
+     FIXME: Is this correct? What if the device goes offline in the mean time?
+  */
+  transferInfoError = 
+    checkAndCalculateTransferInformation( deviceData, count, *f_pos, &transferInformation);
+  
+  if (transferInfoError){
+    return transferInfoError;
+  }
 
   /* now we really want to access, so we need the mutex */
   if (mutex_lock_interruptible(&deviceData->devMutex)) {
@@ -115,11 +166,12 @@ ssize_t mtcaDummy_write_no_struct(struct file *filp, const char __user *buf,
 
   /* not very efficient, but currently the post-write action is per word */
   { unsigned int i;
-    for (i = 0; i < nBytesToTransfer/sizeof(int32_t); ++i){
+    for (i = 0; i < transferInformation.nBytesToTransfer/sizeof(int32_t); ++i){
       /* offset in loop is the additional offset due to the writing process */
       unsigned int offsetInLoop = i*sizeof(int32_t);
       /* adding to the barStart pointer has to be in words, not in bytes */
-      if (copy_from_user((void*)(barStart + offset/sizeof(uint32_t) +i), 
+      if (copy_from_user((void*)(transferInformation.barStart + 
+				 transferInformation.offset/sizeof(uint32_t) +i), 
 			 /* buf is a pointer to char, to here we add the offset in bytes */
 			   buf + offsetInLoop, sizeof(int32_t))){
 	dbg_print("%s\n", "Error in copy_from_user");
@@ -128,11 +180,11 @@ ssize_t mtcaDummy_write_no_struct(struct file *filp, const char __user *buf,
       }
 
       /* invoke the simulation of the "firmware" */
-      if (mtcadummy_performActionOnWrite( offset + offsetInLoop,
-					  bar,
+      if (mtcadummy_performActionOnWrite( transferInformation.offset + offsetInLoop,
+					  transferInformation.bar,
 					  deviceData->slotNr )){
 	dbg_print("Simulating write access to bad register at offset %ld, %s.\n",
-		  offset + offsetInLoop,
+		  transferInformation.offset + offsetInLoop,
 		  "intentinally causing an I/O error");
 	mutex_unlock(&deviceData->devMutex);
 	return -EIO;	  
@@ -147,5 +199,5 @@ ssize_t mtcaDummy_write_no_struct(struct file *filp, const char __user *buf,
 
   mutex_unlock(&deviceData->devMutex);
   
-  return nBytesToTransfer;
+  return transferInformation.nBytesToTransfer;
 }
